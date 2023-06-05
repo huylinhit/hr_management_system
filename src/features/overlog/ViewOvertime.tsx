cd9bd9258b7d5817aa67e90bf7619a2099dee0a6
Bao Ngoc <ngocnguyen.150903@gmail.com>
2023-06-05 15:02:03 +0700

    Update ViewOvertime.tsx

--- src/features/overlog/ViewOvertime.tsx
+++ src/features/overlog/ViewOvertime.tsx
@@ -76,55 +76,55 @@ function ViewOvertimeLog() {
     return (
         <>
             <Container>
-        <Container sx={{ display: "flex", justifyContent: "space-between" }}>
-          <Grid item xs={4}>
-          <div>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Tạo yêu cầu tăng ca
-      </Button>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle sx={headerStyle} color="primary" > Yêu cầu tăng ca</DialogTitle>
-        <DialogContent>
-          <Grid item xs={12} sx={{ py: '8px', border: '4px' }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={2}><Typography sx={headerStyle}>Loại tăng ca</Typography></Grid>
-                        <Grid item xs={10}><Grid item xs={10}>
-                            <FormControl fullWidth>
-                            <InputLabel id="demo-simple-select-label">Không</InputLabel>
-                                    <Select
-                                        labelId="demo-simple-select-label"
-                                        id="demo-simple-select"
-                                        label="Tăng ca thường"
-                                        onChange={handleChange}
-                                    >
-                                        <MenuItem value={10}>Không</MenuItem>
-                                        <MenuItem value={20}>Tang ca thuong</MenuItem>
-                                        <MenuItem value={30}>Khong thuong</MenuItem>
-                                    </Select>
-                            </FormControl>
-                        </Grid>
-                        </Grid>
-                    </Grid>
-                </Grid>
-                <Grid container >
-                    <Grid item xs={12} sx={{ py: '8px', border: '4px' }}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={2}><Typography sx={headerStyle}>Chọn ngày</Typography></Grid>
-                            <Grid item xs={10}>
-                                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                    <DemoContainer components={['DateRangePicker']}>
-                                        <DateRangePicker localeText={{ start: 'Check-in', end: 'Check-out' }} />
-                                    </DemoContainer>
-                                </LocalizationProvider>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Grid>
-                <Grid container >
-                    <Grid item xs={12} sx={{ py: '8px', border: '4px' }}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={2}><Typography sx={headerStyle}>Thời gian</Typography></Grid>
-                            <Grid item xs={10}><TextField id="outlined-basic" label="Outlined" variant="outlined" /></Grid>
+                <Container sx={{ display: "flex", justifyContent: "space-between" }}>
+                    <Grid item xs={4}>
+                        <div>
+                            <Button variant="outlined" onClick={handleClickOpen}>
+                                Tạo yêu cầu tăng ca
+                            </Button>
+                            <Dialog open={open} onClose={handleClose}>
+                                <DialogTitle sx={headerStyle} color="primary" > Yêu cầu tăng ca</DialogTitle>
+                                <DialogContent>
+                                    <Grid item xs={12} sx={{ py: '8px', border: '4px' }}>
+                                        <Grid container spacing={2}>
+                                            <Grid item xs={2}><Typography sx={headerStyle}>Loại tăng ca</Typography></Grid>
+                                            <Grid item xs={10}><Grid item xs={10}>
+                                                <FormControl fullWidth>
+                                                    <InputLabel id="demo-simple-select-label">Không</InputLabel>
+                                                    <Select
+                                                        labelId="demo-simple-select-label"
+                                                        id="demo-simple-select"
+                                                        label="Tăng ca thường"
+                                                        onChange={handleChange}
+                                                    >
+                                                        <MenuItem value={10}>Không</MenuItem>
+                                                        <MenuItem value={20}>Tang ca thuong</MenuItem>
+                                                        <MenuItem value={30}>Khong thuong</MenuItem>
+                                                    </Select>
+                                                </FormControl>
+                                            </Grid>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid container >
+                                        <Grid item xs={12} sx={{ py: '8px', border: '4px' }}>
+                                            <Grid container spacing={2}>
+                                                <Grid item xs={2}><Typography sx={headerStyle}>Chọn ngày</Typography></Grid>
+                                                <Grid item xs={10}>
+                                                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                                        <DemoContainer components={['DateRangePicker']}>
+                                                            <DateRangePicker localeText={{ start: 'Check-in', end: 'Check-out' }} />
+                                                        </DemoContainer>
+                                                    </LocalizationProvider>
+                                                </Grid>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid container >
+                                        <Grid item xs={12} sx={{ py: '8px', border: '4px' }}>
+                                            <Grid container spacing={2}>
+                                                <Grid item xs={2}><Typography sx={headerStyle}>Thời gian</Typography></Grid>
+                                                <Grid item xs={10}><TextField id="outlined-basic" label="Outlined" variant="outlined" /></Grid>
 
                                             </Grid>
                                         </Grid>
@@ -135,33 +135,34 @@ function ViewOvertimeLog() {
                                     <Button onClick={handleClose}>Đóng</Button>
                                 </DialogActions>
                             </Dialog>
+
                         </div>
                     </Grid>
 
-          <Grid item xs={4}>
-            <Button variant="contained" sx={{ width: 200 }}>
-              Tìm kiếm
-            </Button>
-          </Grid>
-          <Grid item xs={2}>
-            <Autocomplete
-              disablePortal
-              id="combo-box-demo"
-              options={top100Films}
-              sx={{ width: 200 }}
-              renderInput={(params) => <TextField {...params} label="Loại" />}
-            />
-          </Grid>
-          <Grid item xs={2}>
-            <Autocomplete
-              disablePortal
-              id="combo-box-demo"
-              options={top100Films}
-              sx={{ width: 200 }}
-              renderInput={(params) => <TextField {...params} label="Phòng" />}
-            />
-          </Grid>
-        </Container>
+                    <Grid item xs={4}>
+                        <Button variant="contained" sx={{ width: 200 }}>
+                            Tìm kiếm
+                        </Button>
+                    </Grid>
+                    <Grid item xs={2}>
+                        <Autocomplete
+                            disablePortal
+                            id="combo-box-demo"
+                            options={top100Films}
+                            sx={{ width: 200 }}
+                            renderInput={(params) => <TextField {...params} label="Loại" />}
+                        />
+                    </Grid>
+                    <Grid item xs={2}>
+                        <Autocomplete
+                            disablePortal
+                            id="combo-box-demo"
+                            options={top100Films}
+                            sx={{ width: 200 }}
+                            renderInput={(params) => <TextField {...params} label="Phòng" />}
+                        />
+                    </Grid>
+                </Container>
 
                 <TableContainer component={Paper}>
                     <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
@@ -196,6 +197,7 @@ function ViewOvertimeLog() {
                             ))}
                         </TableBody>
                     </Table>
+
                 </TableContainer>
             </Container>
         </>
