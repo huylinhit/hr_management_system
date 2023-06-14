4c92e602875bc4ddd767bb28459886f20b927549
louisedoan <doanchaucbt@gmail.com>
2023-06-14 23:36:01 +0700

    HRS-18

--- src/features/overlog/ViewOvertime.tsx
+++ src/features/overlog/ViewOvertime.tsx
@@ -1,203 +1,345 @@
-
 import { ReactNode } from "react";
 
-import { LocalizationProvider } from '@mui/x-date-pickers-pro';
-import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
-import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
-import { SelectChangeEvent, Container, Grid, Button, Dialog, DialogTitle, DialogContent, Typography, FormControl, InputLabel, Select, 
-    MenuItem, TextField, DialogActions, Autocomplete, TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
+import { LocalizationProvider, heIL } from "@mui/x-date-pickers-pro";
+import { AdapterDayjs } from "@mui/x-date-pickers-pro/AdapterDayjs";
+import { DateRangePicker } from "@mui/x-date-pickers-pro/DateRangePicker";
+import {
+  SelectChangeEvent,
+  Container,
+  Grid,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Typography,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  TextField,
+  DialogActions,
+  Autocomplete,
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@mui/material";
 import React from "react";
+import { BorderColor } from "@mui/icons-material";
 
 const headerStyle = {
-    fontWeight: 'bold'
-}
+  fontWeight: "bold",
+};
 const top100Films = [
-    { label: "1", year: 1994 },
-    { label: "2", year: 1972 },
-    { label: "3", year: 1974 },
-    { label: "4", year: 2008 },
-    { label: "5", year: 1957 },
-    { label: "6", year: 1993 },
-    { label: "7", year: 1994 },``
+  { label: "1", year: 1994 },
+  { label: "2", year: 1972 },
+  { label: "3", year: 1974 },
+  { label: "4", year: 2008 },
+  { label: "5", year: 1957 },
+  { label: "6", year: 1993 },
+  { label: "7", year: 1994 },
+  ``,
 ];
 
 function ViewOvertimeLog() {
+  function createData(
+    msnv: string,
+    name: string,
+    kind: string,
+    to: string,
+    from: string,
+    times: string,
+    reason: string,
+    status: string
+  ) {
+    return { msnv, name, kind, to, from, times, reason, status };
+  }
 
-    function createData(
-        name: string,
-        department: string,
-        kind: string,
-        to: string,
-        from: string,
-        times: string,
-        status: string,
-    ) {
-        return { name, department, kind, to, from, times, status };
-    }
+  const rows = [
+    createData(
+      "NV01",
+      "Nguyễn Hồng Ngọc",
+      "Ngày lễ",
+      "09/01/2023 19:00",
+      "09/01/2023 22:00",
+      "3:00",
+      "...",
+      "chờ duyệt"
+    ),
+    createData(
+      "NV01",
+      "Nguyễn Hồng Ngọc",
+      "Ngày lễ",
+      "09/01/2023 19:00",
+      "09/01/2023 22:00",
+      "3:00",
+      "...",
+      "chờ duyệt"
+    ),
+    createData(
+      "NV01",
+      "Nguyễn Hồng Ngọc",
+      "Ngày lễ",
+      "09/01/2023 19:00",
+      "09/01/2023 22:00",
+      "3:00",
+      "...",
+      "chờ duyệt"
+    ),
+    createData(
+      "NV01",
+      "Nguyễn Hồng Ngọc",
+      "Ngày lễ",
+      "09/01/2023 19:00",
+      "09/01/2023 22:00",
+      "3:00",
+      "...",
+      "chờ duyệt"
+    ),
+    createData(
+      "NV01",
+      "Nguyễn Hồng Ngọc",
+      "Ngày lễ",
+      "09/01/2023 19:00",
+      "09/01/2023 22:00",
+      "3:00",
+      "...",
+      "chờ duyệt"
+    ),
+    createData(
+      "NV01",
+      "Nguyễn Hồng Ngọc",
+      "Ngày lễ",
+      "09/01/2023 19:00",
+      "09/01/2023 22:00",
+      "3:00",
+      "...",
+      "chờ duyệt"
+    ),
+    createData(
+      "NV01",
+      "Nguyễn Hồng Ngọc",
+      "Ngày lễ",
+      "09/01/2023 19:00",
+      "09/01/2023 22:00",
+      "3:00",
+      "...",
+      "chờ duyệt"
+    ),
+    createData(
+      "NV01",
+      "Nguyễn Hồng Ngọc",
+      "Ngày lễ",
+      "09/01/2023 19:00",
+      "09/01/2023 22:00",
+      "3:00",
+      "...",
+      "chờ duyệt"
+    ),
+    createData(
+      "NV01",
+      "Nguyễn Hồng Ngọc",
+      "Ngày lễ",
+      "09/01/2023 19:00",
+      "09/01/2023 22:00",
+      "3:00",
+      "...",
+      "chờ duyệt"
+    ),
+    createData(
+      "NV01",
+      "Nguyễn Hồng Ngọc",
+      "Ngày lễ",
+      "09/01/2023 19:00",
+      "09/01/2023 22:00",
+      "3:00",
+      "...",
+      "chờ duyệt"
+    ),
+    createData(
+      "NV01",
+      "Nguyễn Hồng Ngọc",
+      "Ngày lễ",
+      "09/01/2023 19:00",
+      "09/01/2023 22:00",
+      "3:00",
+      "...",
+      "chờ duyệt"
+    ),
+    createData(
+      "NV01",
+      "Nguyễn Hồng Ngọc",
+      "Ngày lễ",
+      "09/01/2023 19:00",
+      "09/01/2023 22:00",
+      "3:00",
+      "...",
+      "chờ duyệt"
+    ),
+    createData(
+      "NV01",
+      "Nguyễn Hồng Ngọc",
+      "Ngày lễ",
+      "09/01/2023 19:00",
+      "09/01/2023 22:00",
+      "3:00",
+      "...",
+      "chờ duyệt"
+    ),
+    createData(
+      "NV01",
+      "Nguyễn Hồng Ngọc",
+      "Ngày lễ",
+      "09/01/2023 19:00",
+      "09/01/2023 22:00",
+      "3:00",
+      "...",
+      "chờ duyệt"
+    ),
+    createData(
+      "NV01",
+      "Nguyễn Hồng Ngọc",
+      "Ngày lễ",
+      "09/01/2023 19:00",
+      "09/01/2023 22:00",
+      "3:00",
+      "...",
+      "chờ duyệt"
+    ),
+    createData(
+      "NV01",
+      "Nguyễn Hồng Ngọc",
+      "Ngày lễ",
+      "09/01/2023 19:00",
+      "09/01/2023 22:00",
+      "3:00",
+      "...",
+      "chờ duyệt"
+    ),
+    createData(
+      "NV01",
+      "Nguyễn Hồng Ngọc",
+      "Ngày lễ",
+      "09/01/2023 19:00",
+      "09/01/2023 22:00",
+      "3:00",
+      "...",
+      "chờ duyệt"
+    ),
+  ];
+  function handleChange(
+    event: SelectChangeEvent<unknown>,
+    child: ReactNode
+  ): void {}
+  const [open, setOpen] = React.useState(false);
 
-    const rows = [
-        createData('Minh Hoang', "Phát triển sản phẩm", "Bệnh", "09/01/2023 19:00", "09/01/2023 22:00", "3:00", "Đang chờ"),
-        createData('Minh Hoang', "Phát triển sản phẩm", "Bệnh", "09/01/2023 19:00", "09/01/2023 22:00", "3:00", "Đang chờ"),
-        createData('Minh Hoang', "Phát triển sản phẩm", "Bệnh", "09/01/2023 19:00", "09/01/2023 22:00", "3:00", "Đang chờ"),
-        createData('Minh Hoang', "Phát triển sản phẩm", "Bệnh", "09/01/2023 19:00", "09/01/2023 22:00", "3:00", "Đang chờ"),
-        createData('Minh Hoang', "Phát triển sản phẩm", "Bệnh", "09/01/2023 19:00", "09/01/2023 22:00", "3:00", "Đang chờ"),
-        createData('Minh Hoang', "Phát triển sản phẩm", "Bệnh", "09/01/2023 19:00", "09/01/2023 22:00", "3:00", "Đang chờ"),
-        createData('Minh Hoang', "Phát triển sản phẩm", "Bệnh", "09/01/2023 19:00", "09/01/2023 22:00", "3:00", "Đang chờ"),
-        createData('Minh Hoang', "Phát triển sản phẩm", "Bệnh", "09/01/2023 19:00", "09/01/2023 22:00", "3:00", "Đang chờ"),
-        createData('Minh Hoang', "Phát triển sản phẩm", "Bệnh", "09/01/2023 19:00", "09/01/2023 22:00", "3:00", "Đang chờ"),
-        createData('Minh Hoang', "Phát triển sản phẩm", "Bệnh", "09/01/2023 19:00", "09/01/2023 22:00", "3:00", "Đang chờ"),
-        createData('Minh Hoang', "Phát triển sản phẩm", "Bệnh", "09/01/2023 19:00", "09/01/2023 22:00", "3:00", "Đang chờ"),
-        createData('Minh Hoang', "Phát triển sản phẩm", "Bệnh", "09/01/2023 19:00", "09/01/2023 22:00", "3:00", "Đang chờ"),
-        createData('Minh Hoang', "Phát triển sản phẩm", "Bệnh", "09/01/2023 19:00", "09/01/2023 22:00", "3:00", "Đang chờ"),
-        createData('Minh Hoang', "Phát triển sản phẩm", "Bệnh", "09/01/2023 19:00", "09/01/2023 22:00", "3:00", "Đang chờ"),
-        createData('Minh Hoang', "Phát triển sản phẩm", "Bệnh", "09/01/2023 19:00", "09/01/2023 22:00", "3:00", "Đang chờ"),
-        createData('Minh Hoang', "Phát triển sản phẩm", "Bệnh", "09/01/2023 19:00", "09/01/2023 22:00", "3:00", "Đang chờ"),
-        createData('Minh Hoang', "Phát triển sản phẩm", "Bệnh", "09/01/2023 19:00", "09/01/2023 22:00", "3:00", "Đang chờ"),
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <Container>
+        <Typography variant="h5" sx={headerStyle}>
+          Danh sách đơn làm thêm giờ
+        </Typography>
+        <Container sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Grid item xs={4}></Grid>
 
+          <Grid item xs={4}>
+            <Button variant="contained" sx={{ width: 200 }}>
+              Tìm kiếm
+            </Button>
+          </Grid>
+          <Grid item xs={2}>
+            <Autocomplete
+              disablePortal
+              id="combo-box-demo"
+              options={top100Films}
+              sx={{ width: 200 }}
+              renderInput={(params) => <TextField {...params} label="Loại" />}
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <Autocomplete
+              disablePortal
+              id="combo-box-demo"
+              options={top100Films}
+              sx={{ width: 200 }}
+              renderInput={(params) => <TextField {...params} label="Phòng" />}
+            />
+          </Grid>
+          <Button variant="outlined" onClick={handleClickOpen}>
+            Tạo yêu cầu tăng ca
+          </Button>
+        </Container>
 
-    ];
-    function handleChange(event: SelectChangeEvent<unknown>, child: ReactNode): void {
-
-    }
-    const [open, setOpen] = React.useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-    return (
-        <>
-            <Container>
-                <Container sx={{ display: "flex", justifyContent: "space-between" }}>
-                    <Grid item xs={4}>
-                        <div>
-                            <Button variant="outlined" onClick={handleClickOpen}>
-                                Tạo yêu cầu tăng ca
-                            </Button>
-                            <Dialog open={open} onClose={handleClose}>
-                                <DialogTitle sx={headerStyle} color="primary" > Yêu cầu tăng ca</DialogTitle>
-                                <DialogContent>
-                                    <Grid item xs={12} sx={{ py: '8px', border: '4px' }}>
-                                        <Grid container spacing={2}>
-                                            <Grid item xs={2}><Typography sx={headerStyle}>Loại tăng ca</Typography></Grid>
-                                            <Grid item xs={10}><Grid item xs={10}>
-                                                <FormControl fullWidth>
-                                                    <InputLabel id="demo-simple-select-label">Không</InputLabel>
-                                                    <Select
-                                                        labelId="demo-simple-select-label"
-                                                        id="demo-simple-select"
-                                                        label="Tăng ca thường"
-                                                        onChange={handleChange}
-                                                    >
-                                                        <MenuItem value={10}>Không</MenuItem>
-                                                        <MenuItem value={20}>Tang ca thuong</MenuItem>
-                                                        <MenuItem value={30}>Khong thuong</MenuItem>
-                                                    </Select>
-                                                </FormControl>
-                                            </Grid>
-                                            </Grid>
-                                        </Grid>
-                                    </Grid>
-                                    <Grid container >
-                                        <Grid item xs={12} sx={{ py: '8px', border: '4px' }}>
-                                            <Grid container spacing={2}>
-                                                <Grid item xs={2}><Typography sx={headerStyle}>Chọn ngày</Typography></Grid>
-                                                <Grid item xs={10}>
-                                                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                                        <Container>
-                                                            <DateRangePicker localeText={{ start: 'Check-in', end: 'Check-out' }} />
-                                                        </Container>
-                                                    </LocalizationProvider>
-                                                </Grid>
-                                            </Grid>
-                                        </Grid>
-                                    </Grid>
-                                    <Grid container >
-                                        <Grid item xs={12} sx={{ py: '8px', border: '4px' }}>
-                                            <Grid container spacing={2}>
-                                                <Grid item xs={2}><Typography sx={headerStyle}>Thời gian</Typography></Grid>
-                                                <Grid item xs={10}><TextField id="outlined-basic" label="Outlined" variant="outlined" /></Grid>
-
-                                            </Grid>
-                                        </Grid>
-                                    </Grid>
-                                </DialogContent>
-                                <DialogActions>
-                                    <Button onClick={handleClose}>Tạo</Button>
-                                    <Button onClick={handleClose}>Đóng</Button>
-                                </DialogActions>
-                            </Dialog>
-
-                        </div>
-                    </Grid>
-
-                    <Grid item xs={4}>
-                        <Button variant="contained" sx={{ width: 200 }}>
-                            Tìm kiếm
-                        </Button>
-                    </Grid>
-                    <Grid item xs={2}>
-                        <Autocomplete
-                            disablePortal
-                            id="combo-box-demo"
-                            options={top100Films}
-                            sx={{ width: 200 }}
-                            renderInput={(params) => <TextField {...params} label="Loại" />}
-                        />
-                    </Grid>
-                    <Grid item xs={2}>
-                        <Autocomplete
-                            disablePortal
-                            id="combo-box-demo"
-                            options={top100Films}
-                            sx={{ width: 200 }}
-                            renderInput={(params) => <TextField {...params} label="Phòng" />}
-                        />
-                    </Grid>
-                </Container>
-
-                <TableContainer component={Paper}>
-                    <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Tên nhân viên</TableCell>
-                                <TableCell align="right">Phòng ban</TableCell>
-                                <TableCell align="right">Loại nghỉ phép</TableCell>
-                                <TableCell align="right">Từ</TableCell>
-                                <TableCell align="right">Đến</TableCell>
-                                <TableCell align="right">Thời gian</TableCell>
-                                <TableCell align="right">Trạng thái</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows.map((row) => (
-                                <TableRow
-                                    key={row.name}
-                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                >
-                                    <TableCell component="th" scope="row">
-                                        {row.name}
-                                    </TableCell>
-                                    <TableCell align="right">{row.department}</TableCell>
-                                    <TableCell align="right">{row.kind}</TableCell>
-                                    <TableCell align="right">{row.to}</TableCell>
-                                    <TableCell align="right">{row.from}</TableCell>
-                                    <TableCell align="right">{row.times}</TableCell>
-                                    <TableCell align="right">{row.status}</TableCell>
-
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-
-                </TableContainer>
-            </Container>
-        </>
-    );
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+            <TableHead>
+              <TableRow sx={{ background: "black" }}>
+                <TableCell sx={{ color: "white" }} align="center">
+                  MSNV
+                </TableCell>
+                <TableCell sx={{ color: "white" }} align="center">
+                  Tên
+                </TableCell>
+                <TableCell sx={{ color: "white" }} align="center">
+                  Loại tăng ca
+                </TableCell>
+                <TableCell sx={{ color: "white" }} align="center">
+                  Từ
+                </TableCell>
+                <TableCell sx={{ color: "white" }} align="center">
+                  Đến
+                </TableCell>
+                <TableCell sx={{ color: "white" }} align="center">
+                  Thời gian
+                </TableCell>
+                <TableCell sx={{ color: "white" }} align="center">
+                  Lý do
+                </TableCell>
+                <TableCell sx={{ color: "white" }} align="center">
+                  Trạng thái
+                </TableCell>
+                <TableCell sx={{ color: "white" }} align="center"></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.msnv}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.msnv}
+                  </TableCell>
+                  <TableCell align="center">{row.name}</TableCell>
+                  <TableCell align="center">{row.kind}</TableCell>
+                  <TableCell align="center">{row.to}</TableCell>
+                  <TableCell align="center">{row.from}</TableCell>
+                  <TableCell align="center">{row.times}</TableCell>
+                  <TableCell align="center">{row.reason}</TableCell>
+                  <TableCell align="center">
+                    <div>{row.status}</div>
+                  </TableCell>
+                  <TableCell align="center">
+                    <BorderColor />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
+    </>
+  );
 }
 
 export default ViewOvertimeLog;
