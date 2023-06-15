0148ba9fc2296faa60ea69300f84267d9753b4fc
Bao Ngoc <ngocnguyen.150903@gmail.com>
2023-06-15 16:55:01 +0700

    Update detail ot

--- src/features/detail_overtime/DetailOvertime.tsx
+++ src/features/detail_overtime/DetailOvertime.tsx
@@ -1,29 +1,43 @@
+import { useState } from "react";
 import { Box, Container, Grid, Typography } from "@mui/material";
 
 //style
-import "./DetailOvertime.css"
+import "./DetailOvertime.css";
 
 // component
 import DetailOvertimeContent from "./component/DetailOvertimeContent";
 import DetailOvertimeFooter from "./component/DetailOvertimeFooter";
 
+// data
+import { STAFF, OTLOG, OTTYPE } from "../../app/store/data";
 
-export default function DetailOvertime () {
-    return (
-        <Box className="page-container">
-            <Grid container className="page-title">
-                <Typography>
-                    Phản hồi đơn làm thêm giờ
-                </Typography>
-            </Grid>
-            <Grid container className="page-content" >
-                <Grid item sx={{ width: "100%", padding: "10px 50px"}}>
-                    <DetailOvertimeContent />
-                </Grid>
-                <Grid item sx={{ width: "100%"}}>
-                    <DetailOvertimeFooter/>
-                </Grid>
-            </Grid>
-        </Box>
-    )
+// api
+import { UserInfor } from "../../app/models/userInfor";
+import { LogOT } from "../../app/models/LogOT";
+import { OtType } from "../../app/models/otType";
+
+export default function DetailOvertime() {
+  // -------------------------- VAR -----------------------------
+  const [logOt, setLogOt] = useState<LogOT>(OTLOG);
+  const [types, setTypes] = useState<OtType[]>(OTTYPE)
+  // -------------------------- STATE ---------------------------
+  const [staff, setStaff] = useState<UserInfor>(STAFF);
+  // -------------------------- REDUX ---------------------------
+  // -------------------------- EFFECT --------------------------
+  // -------------------------- MAIN ----------------------------
+  return (
+    <Box className="page-container">
+      <Grid container className="page-title">
+        <Typography>Phản hồi đơn làm thêm giờ</Typography>
+      </Grid>
+      <Grid container className="page-content">
+        <Grid item sx={{ width: "100%", padding: "20px 70px" }}>
+          <DetailOvertimeContent logOt={logOt} staff={staff} types={types} />
+        </Grid>
+        <Grid item sx={{ width: "100%" }}>
+          <DetailOvertimeFooter />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
--- src/features/detail_overtime/component/DetailOvertimeContent.tsx
+++ src/features/detail_overtime/component/DetailOvertimeContent.tsx
@@ -1,36 +1,246 @@
-import { Container, Grid, Typography } from "@mui/material";
+import {
+  Container,
+  Grid,
+  InputLabel,
+  MenuItem,
+  TextField,
+  Typography,
+  Select,
+  OutlinedInput,
+  FormControl,
+} from "@mui/material";
+import { useState } from "react";
+import { SelectChangeEvent } from "@mui/material/Select";
 
-export default function DetailOvertimeContent() {
+// api
+import { UserInfor } from "../../../app/models/userInfor";
+import { LogOT } from "../../../app/models/LogOT";
+import { OtType } from "../../../app/models/otType";
+
+// interface
+interface Props {
+  staff: UserInfor;
+  logOt: LogOT;
+  types: OtType[];
+}
+
+export default function DetailOvertimeContent({ staff, logOt, types }: Props) {
   // -------------------------- VAR -----------------------------
-  const formField = [
-    "Mã đơn: ",
-    "Mã số nhân viên: ",
-    "Tên nhân viên: ",
-    "Loại đơn: ",
-    "Từ: ",
-    "Đến: ",
-    "Số giờ làm: ",
-    "Nội dung đơn: ",
-    "Trạng thái: ",
-    "Ngày gửi đơn: ",
-    "Phản hồi: ",
-  ];
+  console.log(logOt);
+  console.log(staff);
+  const type = types.find((type) => type.otTypeId === logOt.otTypeId);
+  const ticketStatus = ["Chờ duyệt", "Đồng ý", "Từ chối"];
   // -------------------------- STATE ---------------------------
+  const [disable, setDisable] = useState(false);
+  const [selectedType, setSelectedType] = useState("");
   // -------------------------- REDUX ---------------------------
   // -------------------------- EFFECT --------------------------
+  // -------------------------- FUNCTION ------------------------
+  const handleChange = (event: SelectChangeEvent) => {
+    setSelectedType(event.target.value as string);
+  };
   // -------------------------- MAIN ----------------------------
   return (
-    <Grid container 
-      sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-    >
-      <Grid item xs={4} className="form-title">
-        {formField.map((field) => (
-          <Typography>{field}</Typography>
-        ))}
+    <>
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          marginBottom: "10px",
+        }}
+      >
+        <Grid item xs={4} className="form-title">
+          <Typography>Mã đơn: </Typography>
+        </Grid>
+        <Grid item xs={8} className="form-content">
+          <Typography>{logOt.otLogId}</Typography>
+        </Grid>
       </Grid>
-      <Grid item xs={8} className="form-content">
-        <Typography>abc</Typography>
+
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          marginBottom: "10px",
+        }}
+      >
+        <Grid item xs={4} className="form-title">
+          <Typography>Mã số nhân viên: </Typography>
+        </Grid>
+        <Grid item xs={8} className="form-content">
+          <Typography>{logOt.staffId}</Typography>
+        </Grid>
       </Grid>
-    </Grid>
+
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          marginBottom: "10px",
+        }}
+      >
+        <Grid item xs={4} className="form-title">
+          <Typography>Tên nhân viên: </Typography>
+        </Grid>
+        <Grid item xs={8} className="form-content">
+          <Typography>
+            {staff.lastName} {staff.firstName}
+          </Typography>
+        </Grid>
+      </Grid>
+
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          marginBottom: "10px",
+        }}
+      >
+        <Grid item xs={4} className="form-title">
+          <Typography>Loại đơn: </Typography>
+        </Grid>
+        <Grid item xs={8} className="form-content">
+          <Typography>{type?.typeName}</Typography>
+        </Grid>
+      </Grid>
+
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          marginBottom: "10px",
+        }}
+      >
+        <Grid item xs={4} className="form-title">
+          <Typography>Từ: </Typography>
+        </Grid>
+        <Grid item xs={8} className="form-content">
+          <Typography>{logOt.logStart}</Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          marginBottom: "10px",
+        }}
+      >
+        <Grid item xs={4} className="form-title">
+          <Typography>Đến: </Typography>
+        </Grid>
+        <Grid item xs={8} className="form-content">
+          <Typography>{logOt.logEnd}</Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          marginBottom: "10px",
+        }}
+      >
+        <Grid item xs={4} className="form-title">
+          <Typography>Số giờ làm: </Typography>
+        </Grid>
+        <Grid item xs={8} className="form-content">
+          <Typography>{logOt.logHours}</Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          marginBottom: "10px",
+        }}
+      >
+        <Grid item xs={4} className="form-title">
+          <Typography>Nội dung đơn: </Typography>
+        </Grid>
+        <Grid item xs={8} className="form-content">
+          <Typography>{logOt.reason}</Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          marginBottom: "10px",
+        }}
+      >
+        <Grid item xs={4} className="form-title">
+          <Typography>Ngày gửi đơn: </Typography>
+        </Grid>
+        <Grid item xs={8} className="form-content">
+          <Typography>{logOt.createAt}</Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          marginBottom: "10px",
+        }}
+      >
+        <Grid item xs={4} className="form-title">
+          <Typography>Trạng thái: </Typography>
+        </Grid>
+        <Grid item xs={8} className="form-content">
+          <FormControl sx={{ m: 1, minWidth: 200 }} size="small">
+            <Select
+              labelId="demo-select-small-label"
+              id="demo-select-small"
+              value={selectedType}
+              label="Status"
+              onChange={handleChange}
+            >
+              {ticketStatus.map((status) => (
+                <MenuItem value={status}>{status}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          marginBottom: "10px",
+        }}
+      >
+        <Grid item xs={4} className="form-title">
+          <Typography>Phản hồi: </Typography>
+        </Grid>
+        <Grid item xs={8} className="form-content">
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Nhập phản hồi..."
+            multiline
+            rows={4}
+          />
+        </Grid>
+      </Grid>
+    </>
   );
 }
