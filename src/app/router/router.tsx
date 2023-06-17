d2fd6c38fa75af3782ac35ed79717839d0d6616f
Bao Ngoc <ngocnguyen.150903@gmail.com>
2023-06-17 08:11:46 +0700

    Fix conflict

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -4,17 +4,24 @@ import HomePage from "../../features/home/HomePage";
 import ServerErrorPage from "../errors/ServerErrorPage";
 import NotFound from "../errors/NotFound";
 
-import OvertimeLog from "../../features/overlog/OvertimeList";
-import LeavetimeList from "../../features/leavelog/LeavetimeList";
-import MyLeavetime from "../../features/leavelog/MyLeavetime";
-import TypeLeavetime from "../../features/leavelog/TypeLeavetime";
-import ViewOvertime from "../../features/overlog/ViewOvertime";
-import DepartmentPage from "../../features/department/DepartmentPage";
+// component
 import Login from "../../features/account/Login";
 import Register from "../../features/account/Register";
-import DetailOvertime from "../../features/detail_overtime/DetailOvertime";
-import DetailLeave from "../../features/detail_leavelog/DetailLeave";
-import DetailTicket from "../../features/detail_ticket/DetailTicket";
+
+//
+import ViewOvertime from "../../features/overlog/ViewOvertime";
+
+import OvertimeLog from "../../features/overlog/OvertimeList";
+
+//
+import MyLeavetime from "../../features/leavelog/MyLeavetime";
+
+//
+import ViewOtherTypes from "../../features/othertypes/ViewOtherTypes";
+
+//
+import DepartmentDetail from "../../features/department/DepartmentDetail";
+import DepartmentList from "../../features/department/DepartmentList";
 
 export const router = createBrowserRouter([
   {
@@ -27,18 +34,16 @@ export const router = createBrowserRouter([
       { path: "register", element: <Register /> },
 
       //Them router ngay vi tri nay nhe!!
-      { path: '/department-list' , element: <DepartmentPage/>},
-
-      
+      { path: "/overtime", element: <OvertimeLog /> },
       { path: "/viewot", element: <ViewOvertime /> },
-      { path: "/detail-overtime", element: <DetailOvertime /> },
 
       { path: "/myleavelist", element: <MyLeavetime /> },
-      { path: "/leavetime", element: <LeavetimeList /> },
-      { path: "/typeleavetime", element: <TypeLeavetime /> },
-      { path: "/detail-leave-log", element: <DetailLeave /> },
 
-      { path: "/detail-ticket", element: <DetailTicket /> },
+      { path: '/viewothertypes' , element: <ViewOtherTypes/>},
+
+      { path: '/department-list' , element: <DepartmentList/>},
+      { path: '/department-detail' , element: <DepartmentDetail />},
+
       { path: "server-error", element: <ServerErrorPage /> },
       { path: "not-found", element: <NotFound /> },
       { path: "*", element: <Navigate replace to="not-found" /> },
