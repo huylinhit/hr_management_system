5f0ecb356c1af0c84f562b7685c0a7cc9cde97d5
hoangvm <vominhhoangkhung@gmail.com>
2023-07-17 19:30:24 +0700

    update fields

--- src/features/othertypes/MyTicketDetailSkeleton.tsx
+++ src/features/othertypes/MyTicketDetailSkeleton.tsx
@@ -4,79 +4,158 @@ export default function MyTicketDetailSkeleon() {
   return (
     <Box sx={{ minHeight: "1200px" }}>
       <Container sx={{ padding: "2%", width: "60%", borderRadius: "8px" }}>
-        <Skeleton variant="circular" animation="wave" width={200} height={200} sx={{mb:"30px"}} /> {/* Placeholder for avatar */}
+        <Skeleton
+          variant="circular"
+          animation="wave"
+          width={200}
+          height={200}
+          sx={{ mb: "30px" }}
+        />{" "}
+        {/* Placeholder for avatar */}
         <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "80px" }} />
         {/* Placeholder for name */}
-        <Grid container xs={12} spacing={4}>
+        <Grid container spacing={4}>
           <Grid item xs={3}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
           <Grid item xs={9}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
         </Grid>
-        <Grid container xs={12} spacing={4}>
+        <Grid container spacing={4}>
           <Grid item xs={3}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
           <Grid item xs={9}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
         </Grid>
-        <Grid container xs={12} spacing={4}>
+        <Grid container spacing={4}>
           <Grid item xs={3}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
           <Grid item xs={9}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
         </Grid>
-        <Grid container xs={12} spacing={4}>
+        <Grid container spacing={4}>
           <Grid item xs={3}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
           <Grid item xs={9}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
         </Grid>
-        <Grid container xs={12} spacing={4}>
+        <Grid container spacing={4}>
           <Grid item xs={3}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
           <Grid item xs={9}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
         </Grid>
-        <Grid container xs={12} spacing={4}>
+        <Grid container spacing={4}>
           <Grid item xs={3}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
           <Grid item xs={9}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
         </Grid>
-        <Grid container xs={12} spacing={4}>
+        <Grid container spacing={4}>
           <Grid item xs={3}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
           <Grid item xs={9}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
         </Grid>
-        <Grid container xs={12} spacing={4}>
+        <Grid container spacing={4}>
           <Grid item xs={3}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
           <Grid item xs={9}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
         </Grid>
-        <Grid container xs={12} spacing={4}>
+        <Grid container spacing={4}>
           <Grid item xs={3}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
           <Grid item xs={9}>
-            <Skeleton variant="text" animation="wave" sx={{ width: "100%", height: "43px", mb:"10px" }} />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100%", height: "43px", mb: "10px" }}
+            />
           </Grid>
         </Grid>
       </Container>
